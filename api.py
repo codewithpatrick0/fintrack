cca8b063c281a8958e42a2cd9e7d3e015280c86a
Patrick <patrickcabrera2025@gmail.com>
2026-06-20 18:38:00 -0500

    post transacciones añadido

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import psycopg2
 from conexion import str_conexion
-from transacciones_modelo import Transacciones
+from transacciones_modelo import TransaccionLeer, TransaccionCrear
 
 app = FastAPI()
 
-@app.get('/transacciones', response_model=list[Transacciones])
+@app.get('/transacciones', response_model=list[TransaccionLeer])
 def obtener_transacciones():
 
     conexion = psycopg2.connect(str_conexion)
@@ -16,7 +16,7 @@ def obtener_transacciones():
     cursor.execute(consulta)
     filas = cursor.fetchall()
 
-    transacciones = [Transacciones(
+    transacciones = [TransaccionLeer(
         id=fila[0],
         id_usuario=fila[1],
         id_categoria=fila[2],
@@ -32,7 +32,45 @@ def obtener_transacciones():
     
     return transacciones
 
-@app.post('/transacciones', response_model=Transacciones)
-def crear_transaccion(transaccion: Transacciones):
+@app.post('/transacciones', response_model=TransaccionLeer) #sale
+def crear_transaccion(transaccion: TransaccionCrear): #entra
     
-    datos_transaccion = transaccion.model_dump()
+    id_usuario_input = transaccion.id_usuario
+    id_categoria_input = transaccion.id_categoria
+    tipo_movimiento_input = transaccion.tipo_movimiento
+    monto_input = transaccion.monto
+    fuente_input = transaccion.fuente
+    info_input = transaccion.info
+
+    if monto_input <= 0:
+        raise HTTPException(status_code=400, detail="El monto debe ser mayor a 0")
+    
+    if id_categoria_input is None:
+        raise HTTPException(status_code=400, detail="La categoria no puede estar vacía")
+    
+    if tipo_movimiento_input not in ['gasto', 'ingreso', 'ahorro']:
+        raise HTTPException(status_code=400, detail="El tipo de movimiento ingresado no es válido")
+    conexion = psycopg2.connect(str_conexion)
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        INSERT INTO transacciones(id_usuario, id_categoria, tipo_movimiento, monto, fuente, info)
+        VALUES (%s, %s, %s, %s, %s, %s) RETURNING id;
+    """, (id_usuario_input, id_categoria_input, tipo_movimiento_input,
+           monto_input, fuente_input, info_input))
+    resultado = cursor.fetchone()
+    id_generado = resultado[0] if resultado else None
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+    return TransaccionLeer(
+        id=id_generado,
+        id_usuario=id_usuario_input,
+        id_categoria=id_categoria_input,
+        tipo_movimiento=tipo_movimiento_input,
+        monto=monto_input,
+        fuente=fuente_input,
+        info=info_input
+    )
--- transacciones_modelo.py
+++ transacciones_modelo.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-class Transacciones(BaseModel):
+class TransaccionLeer(BaseModel):
     id: int
     id_usuario: int
     id_categoria: int
@@ -8,3 +8,11 @@ class Transacciones(BaseModel):
     monto: float
     fuente: str | None=None
     info: str | None=None
+
+class TransaccionCrear(BaseModel):
+    id_usuario: int
+    id_categoria: int
+    tipo_movimiento: str
+    monto: float
+    fuente: str | None = None
+    info: str | None = None
